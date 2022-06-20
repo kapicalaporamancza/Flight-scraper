673032d9468f43e154413095f11594d37acec12f
kapicalaporamancza <260468@student.pwr.edu.pl>
2022-06-20 16:52:35 +0200

    functions
    searching ready to use with identified problem with date

--- kayak.py
+++ kayak.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 import pandas as pd
+import openpyxl
 
 
 link = "https://www.kayak.pl/flights"
@@ -49,7 +50,7 @@ def dep_date_chooser(dep_date):
     date_box.click()
     time.sleep(5)
     date_button = driver.find_element(
-        by=By.XPATH, value="//div[@aria-label='18 lip 2022']"
+        by=By.XPATH, value="//div[@aria-label='19 lip 2022']"
     )
     date_button.click()
 
@@ -87,167 +88,91 @@ def best_results():
 
 def get_flight_data(index, index4, index8, df):
 
-    print("\n" + "result " + str(index + 1) + "\n")
-
-    # flight 1
-    print("flight 1:")
-    dep_time_1 = driver.find_elements(
-        by=By.XPATH, value="//span[@class='depart-time base-time']"
-    )
-    print(dep_time_1[index4].text)
-
-    arr_time_1 = driver.find_elements(
-        by=By.XPATH, value="//span[@class='arrival-time base-time']"
-    )
-    print(arr_time_1[index4].text)
-
-    airport_1 = driver.find_elements(by=By.XPATH, value="//span[@class='airport-name']")
-    print(airport_1[index8].text)
-
-    airport_2 = driver.find_elements(by=By.XPATH, value="//span[@class='airport-name']")
-    print(airport_2[index8 + 1].text)
-
-    stops_1 = driver.find_elements(
-        by=By.XPATH, value="//span[contains(@class,'stops-text')]"
-    )
-    print(stops_1[index4].text)
-
-    time_1 = driver.find_elements(
-        by=By.XPATH,
-        value="//div[contains(@class,'duration') and contains(@class,'section')]/*[1]",
-    )
-    print(time_1[index4].text)
-
-    # flight 2
-    print("flight 2:")
-    dep_time_1 = driver.find_elements(
-        by=By.XPATH, value="//span[@class='depart-time base-time']"
-    )
-    print(dep_time_1[index4 + 1].text)
-
-    arr_time_1 = driver.find_elements(
-        by=By.XPATH, value="//span[@class='arrival-time base-time']"
-    )
-    print(arr_time_1[index4 + 1].text)
-
-    airport_1 = driver.find_elements(by=By.XPATH, value="//span[@class='airport-name']")
-    print(airport_1[index8 + 2].text)
-
-    airport_2 = driver.find_elements(by=By.XPATH, value="//span[@class='airport-name']")
-    print(airport_2[index8 + 3].text)
-
-    stops_1 = driver.find_elements(
-        by=By.XPATH, value="//span[contains(@class,'stops-text')]"
-    )
-    print(stops_1[index4 + 1].text)
-
-    time_1 = driver.find_elements(
-        by=By.XPATH,
-        value="//div[contains(@class,'duration') and contains(@class,'section')]/*[1]",
-    )
-    print(time_1[index4 + 1].text)
-
-    # airlines
-    airlines = driver.find_elements(
-        by=By.XPATH, value="//span[@class='codeshares-airline-names']"
-    )
-    print(airlines[index].text)
-
-    # price
-    price = driver.find_elements(by=By.XPATH, value="//span[@class='price-text']")
-    print(price[index].text)
-    time.sleep(1)
-
-
-def get_flight_data1(index, index4, index8, df):
-
     l = list()
-    print("\n" + "result " + str(index + 1) + "\n")
+    # print("\n" + "result " + str(index + 1) + "\n")
 
     # flight 1
-    print("flight 1:")
+    # print("flight 1:")
     dep_time_1 = driver.find_elements(
         by=By.XPATH, value="//span[@class='depart-time base-time']"
     )
-    print(dep_time_1[index4].text)
+    # print(dep_time_1[index4].text)
     l.append(dep_time_1[index4].text)
 
     arr_time_1 = driver.find_elements(
         by=By.XPATH, value="//span[@class='arrival-time base-time']"
     )
-    print(arr_time_1[index4].text)
+    # print(arr_time_1[index4].text)
     l.append(arr_time_1[index4].text)
 
     airport_1 = driver.find_elements(by=By.XPATH, value="//span[@class='airport-name']")
-    print(airport_1[index8].text)
+    # print(airport_1[index8].text)
     l.append(airport_1[index8].text)
 
     airport_2 = driver.find_elements(by=By.XPATH, value="//span[@class='airport-name']")
-    print(airport_2[index8 + 1].text)
+    # print(airport_2[index8 + 1].text)
     l.append(airport_2[index8 + 1].text)
 
     stops_1 = driver.find_elements(
         by=By.XPATH, value="//span[contains(@class,'stops-text')]"
     )
-    print(stops_1[index4].text)
+    # print(stops_1[index4].text)
     l.append(stops_1[index4].text)
 
     time_1 = driver.find_elements(
         by=By.XPATH,
         value="//div[contains(@class,'duration') and contains(@class,'section')]/*[1]",
     )
-    print(time_1[index4].text)
+    # print(time_1[index4].text)
     l.append(time_1[index4].text)
 
     # flight 2
-    print("flight 2:")
+    # print("flight 2:")
     dep_time_1 = driver.find_elements(
         by=By.XPATH, value="//span[@class='depart-time base-time']"
     )
-    print(dep_time_1[index4 + 1].text)
+    # print(dep_time_1[index4 + 1].text)
     l.append(dep_time_1[index4 + 1].text)
 
     arr_time_1 = driver.find_elements(
         by=By.XPATH, value="//span[@class='arrival-time base-time']"
     )
-    print(arr_time_1[index4 + 1].text)
+    # print(arr_time_1[index4 + 1].text)
     l.append(arr_time_1[index4 + 1].text)
 
     airport_1 = driver.find_elements(by=By.XPATH, value="//span[@class='airport-name']")
-    print(airport_1[index8 + 2].text)
+    # print(airport_1[index8 + 2].text)
     l.append(airport_1[index8 + 2].text)
 
     airport_2 = driver.find_elements(by=By.XPATH, value="//span[@class='airport-name']")
-    print(airport_2[index8 + 3].text)
+    # print(airport_2[index8 + 3].text)
     l.append(airport_2[index8 + 3].text)
 
     stops_1 = driver.find_elements(
         by=By.XPATH, value="//span[contains(@class,'stops-text')]"
     )
-    print(stops_1[index4 + 1].text)
+    # print(stops_1[index4 + 1].text)
     l.append(stops_1[index4 + 1].text)
 
     time_1 = driver.find_elements(
         by=By.XPATH,
         value="//div[contains(@class,'duration') and contains(@class,'section')]/*[1]",
     )
-    print(time_1[index4 + 1].text)
+    # print(time_1[index4 + 1].text)
     l.append(time_1[index4 + 1].text)
 
     # airlines
     airlines = driver.find_elements(
         by=By.XPATH, value="//span[@class='codeshares-airline-names']"
     )
-    print(airlines[index].text)
+    # print(airlines[index].text)
     l.append(airlines[index].text)
 
     # price
     price = driver.find_elements(by=By.XPATH, value="//span[@class='price-text']")
-    print(price[index].text)
+    # print(price[index].text)
     l.append(price[index].text)
-    time.sleep(1)
 
-    print(l)
     df.loc[df.shape[0]] = l
 
 
@@ -279,7 +204,7 @@ def process_results():
     index8 = 0
 
     while index < 15:
-        get_flight_data(index, index4, index8)
+        get_flight_data(index, index4, index8, df)
         index += 1
         index4 += 4
         index8 += 8
@@ -287,7 +212,7 @@ def process_results():
     time.sleep(5)
 
     while index < 30:
-        get_flight_data(index, index4, index8)
+        get_flight_data(index, index4, index8, df)
         index += 1
         index4 += 4
         index8 += 8
@@ -295,12 +220,13 @@ def process_results():
     time.sleep(5)
 
     while index < 45:
-        get_flight_data(index, index4, index8)
+        get_flight_data(index, index4, index8, df)
         index += 1
         index4 += 4
         index8 += 8
-    load_more()
-    time.sleep(5)
+
+    file_name = "name" + ".xlsx"
+    df.to_excel(file_name, sheet_name="sheet1")
 
     time.sleep(500)
 
@@ -311,7 +237,7 @@ closeCookie()
 time.sleep(random.uniform(0.5, 2))
 dep_country_chooser("waw")
 time.sleep(random.uniform(0.5, 2))
-ret_country_chooser("tos")
+ret_country_chooser("lon")
 time.sleep(random.uniform(0.5, 2))
 dep_date_chooser(1)
 time.sleep(random.uniform(0.5, 2))
@@ -319,28 +245,7 @@ ret_date_chooser(1)
 time.sleep(random.uniform(0.5, 2))
 submit_search()
 time.sleep(30)
-# process_results()
-
-columns = (
-    "dep_time_1",
-    "arr_time_1",
-    "airport_1_1",
-    "airport_1_2",
-    "stops_1",
-    "time_1",
-    "dep_time_2",
-    "arr_time_2",
-    "airport_2_1",
-    "airport_2_2",
-    "stops_2",
-    "time_2",
-    "airlines",
-    "prize",
-)
-df = pd.DataFrame(columns=columns)
-print(df)
-get_flight_data1(0, 0, 0, df)
-print(df)
+process_results()
 time.sleep(20)
 
 
